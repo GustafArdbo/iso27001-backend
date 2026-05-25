a5f0c370847c2d1bed60207aecfa04d2db1cb380
gustafardbo <gustaf.ardbo@gmail.com>
2026-05-25 23:16:21 +0200

    Changed filestructure to spring boot

---
+++ src/main/java/se/iso27001platform/iso27001backend/Iso27001backendApplication.java
@@ -0,0 +1,13 @@
+package se.iso27001platform.iso27001backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iso27001backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iso27001backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iso27001platform/iso27001backend/Iso27001backendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iso27001platform.iso27001backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iso27001backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
